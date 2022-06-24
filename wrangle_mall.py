32c010a260025ddecca4644d1113b5cbd925d8cb
Jason-R-Turner <jason.r.turner22@gmail.com>
2022-06-23 22:30:46 -0500

    adding untracked files

---
+++ wrangle_mall.py
@@ -0,0 +1,15 @@
+### IMPORTS ####
+import pandas as pd
+import seaborn as sns
+from sklearn.preprocessing import MinMaxScaler
+from sklearn.model_selection import train_test_split
+
+import env
+
+def acquire():
+    
+
+    
+def one_hot_encode(df):
+    df['is_female'] - df.gender == 'female'
+    drop
